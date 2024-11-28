dc397be6ecf53ee76ac0d14ba504d6287110cea8
adrigollum <adrienchateau@outlook.fr>
2024-11-28 23:03:02 +0100

    plein de chose

--- Assets/Script/Follow.cs
+++ Assets/Script/Follow.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Follow : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class Follow : MonoBehaviour
 	Rigidbody rb;
 
 	private float dify;
+
+	private float secondsLeft;
 	private float difx;
 	// Start is called before the first frame update
 	void Start()
@@ -18,9 +21,31 @@ public class Follow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+		if (waf.transform.position.y >= -125)
+		{
+
+		
 		dify = waf.transform.position.y - 6 - transform.position.y;
 		difx = waf.transform.position.x - transform.position.x;
 		gameObject.transform.Translate(Vector3.up * dify);
-		/*gameObject.transform.Translate(Vector3.right * difx);*/
+			/*gameObject.transform.Translate(Vector3.right * difx);*/
+
+		}
+		else
+		{
+			StartCoroutine(DelayLoadlevel(2));
+		}
+	}
+
+	IEnumerator DelayLoadlevel(float seconds)
+	{
+		yield return new WaitForSeconds(2);
+		secondsLeft = seconds;
+		do
+		{
+			yield return new WaitForSeconds(2);
+		} while (--secondsLeft > 0);
+
+		SceneManager.LoadScene("Menu", LoadSceneMode.Single);
 	}
 }
