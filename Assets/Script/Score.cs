768e62f8f981040a68b729ea26fcf144bb752d53
adrigollum <adrienchateau@outlook.fr>
2024-11-28 17:43:04 +0100

    recup score

--- Assets/Script/Score.cs
+++ Assets/Script/Score.cs
@@ -6,7 +6,7 @@ using TMPro;
 public class Score : MonoBehaviour
 {
 	[SerializeField] TextMeshProUGUI texte;
-	public int score = 0;
+	public static int score = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +17,7 @@ public class Score : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+		
 		texte.text = score.ToString();
 	}
 }
