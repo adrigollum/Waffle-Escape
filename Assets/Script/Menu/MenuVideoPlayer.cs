1bb73e3826565f80c1604d490f9eb55f442199e4
Kieran Mignon <62752413+Lujac@users.noreply.github.com>
2024-11-29 21:45:19 +0100

    Build + Script folder sorted

---
+++ Assets/Script/Menu/MenuVideoPlayer.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class MenuVideoManager : MonoBehaviour
+{
+    public VideoPlayer introVideo; 
+    public VideoPlayer menuVideo; 
+    public VideoPlayer transitionVideo; 
+
+    private bool isTransitioning = false; 
+    private string nextScene; 
+    void Start()
+{
+    
+    introVideo.Play();
+    introVideo.loopPointReached += EndIntroVideo; 
+}
+
+ void EndIntroVideo(VideoPlayer vp)
+{
+   
+    ShowMenu();
+}
+
+void ShowMenu()
+{
+    
+    menuVideo.Play();
+}
+
+public void OnButtonClick(string action)
+{
+ 
+    menuVideo.Stop(); 
+    transitionVideo.Play(); 
+
+ 
+    if (action == "PlayGame")
+    {
+        transitionVideo.loopPointReached += PlayGame; 
+    }
+    else if (action == "PlayCredits")
+    {
+        transitionVideo.loopPointReached += PlayCredits;
+}
+}
+void PlayGame(VideoPlayer vp)
+{
+    SceneManager.LoadScene("Test");
+}
+
+void PlayCredits(VideoPlayer vp)
+{
+    SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+} 
+}
