40ee3a2f28f45a9243605087b119c81818d279d6
Kieran Mignon <62752413+Lujac@users.noreply.github.com>
2025-01-07 14:10:46 +0100

    VIdeoUpdated

---
+++ Assets/Script/Menu/GetVideo.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class GetVideo : MonoBehaviour
+{
+    [SerializeField] string videoFileName; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetVideoURL();
+    }
+
+    void GetVideoURL() 
+    {
+        VideoPlayer videoPlayer = GetComponent<VideoPlayer>();
+
+        if (videoPlayer)
+        {
+            string videoPath = System.IO.Path.Combine(Application.dataPath, "Graph", "2D", "Video", "Menu", videoFileName);
+            Debug.Log(videoPath); 
+            videoPlayer.url = videoPath; 
+        }
+    }
+}
